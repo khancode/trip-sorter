9b43185f19fc85439753eeec35e53664761b907e
Omar Khan <khancodegt@gmail.com>
2016-02-14 16:51:43 -0500

    connect UI to dijkstra's algo

--- app/app.js
+++ app/app.js
@@ -13,13 +13,53 @@ myApp.controller('mainController', ['$scope', 'SORTING_TYPES', function($scope, 
 
     $scope.deals = response.deals;
     $scope.SORTING_TYPES = SORTING_TYPES;
-    $scope.cities = setCities();
+    $scope.cities = getCities($scope.deals);
+    var dealReferenceMap = getDealReferenceMap($scope.deals);
+    $scope.trips;
 
-    function setCities() {
+    $scope.submitForm = function() {
+
+        validate($scope.fromCity, $scope.toCity);
+
+        $scope.trips = findTrips($scope.deals, 'Cheapest', $scope.fromCity, $scope.toCity);
+    };
+
+    function validate(fromCity, toCity) {
+        //TODO: need to update UI for input validation
+        if (!fromCity || !toCity)
+            throw 'Please select from and to cities';
+    }
+
+    function findTrips(deals, sortingType, source, dest) {
+        // Run Dijkstra's shortest path algo
+        var shortestPathObj = $dijkstra.run(deals, sortingType, source, dest);
+
+        // Now extract the shortest path from source -> dest
+        var trips = [];
+        var arrivalCity = dest;
+        var departureCity = shortestPathObj.prev[arrivalCity];
+        while (departureCity != undefined) {
+            console.log('departureCity: ' + departureCity);
+            console.log('arrivalCity: ' + arrivalCity);
+
+            trips.push(dealReferenceMap[shortestPathObj.referenceDealMap[departureCity + arrivalCity]]);
+            arrivalCity = departureCity;
+            departureCity = shortestPathObj.prev[arrivalCity];
+        }
+
+        trips.reverse();
+
+        console.log('dat final destination doe');
+        console.log(trips);
+
+        return trips;
+    }
+
+    function getCities(deals) {
         var citiesHashmap = {};
         var cities = [];
-        for (var i in $scope.deals) {
-            var deal = $scope.deals[i];
+        for (var i in deals) {
+            var deal = deals[i];
 
             if (!citiesHashmap[deal.departure]) {
                 citiesHashmap[deal.departure] = 1;
@@ -36,6 +76,13 @@ myApp.controller('mainController', ['$scope', 'SORTING_TYPES', function($scope, 
         return cities;
     }
 
+    function getDealReferenceMap(deals) {
+        var dealsMap = [];
+        for (var i in deals) {
+            dealsMap[deals[i].reference] = deals[i];
+        }
 
+        return dealsMap;
+    }
 
 }]);
